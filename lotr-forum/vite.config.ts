8a8ac71349247f496842d4e13fab01e941e497ab
Tanner B. Hess Webber <tb@hesswebber.com>
2024-01-08 13:56:38 -0500

    build(deps): fixes dependency install location

--- lotr-forum/vite.config.ts
+++ lotr-forum/vite.config.ts
@@ -10,7 +10,9 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@composables': fileURLToPath(new URL('./src/composables', import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     }
   }
 })
